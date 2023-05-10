de15dde1e514fd69e7f68406e5d3fc9e4fecada8
Surendra Chakravarthula <surendra.chakravarthula.24.1@mountblue.tech>
2023-05-10 10:00:01 +0530

    Add deliveries table

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ createUserAndDb.sql creates a user named surendra and a database named iplprojec
 cleanUp.sql drops the database that was created previously and also drops the user.
 
 # loadCsvData.sql
-loadCsvData.sql creates the table named matches and copies the data from csv file into the created table. File stored in /tmp folder because server can access the files that are in /tmp folder.
+loadCsvData.sql creates the table named matches and deliveries. Copies the data from csv file into the created tables. Files stored in /tmp folder because server can access the files that are in /tmp folder directly.
 
 # ipl_project.sql
 ipl_project.sql contains four queries that reads data from two csv files named matches.csv and deliveries.csv.
--- loadCsvData.sql
+++ loadCsvData.sql
@@ -1,3 +1,4 @@
+--create matches table and copy data from matches.csv
 CREATE TABLE matches (
   id int DEFAULT NULL,
   season int DEFAULT NULL,
@@ -21,4 +22,33 @@ CREATE TABLE matches (
 
 COPY matches
 FROM '/tmp/matches.csv'
+WITH (FORMAT csv, HEADER true);
+
+--create deliveries and copy data from delievries.csv
+CREATE TABLE deliveries (
+  match_id int DEFAULT NULL,
+  inning int DEFAULT NULL,
+  batting_team text,
+  bowling_team text,
+  over int DEFAULT NULL,
+  ball int DEFAULT NULL,
+  batsman text,
+  non_striker text,
+  bowler text,
+  is_super_over double DEFAULT NULL,
+  wide_runs int DEFAULT NULL,
+  bye_runs int DEFAULT NULL,
+  legbye_runs int DEFAULT NULL,
+  noball_runs int DEFAULT NULL,
+  penalty_runs int DEFAULT NULL,
+  batsman_runs int DEFAULT NULL,
+  extra_runs int DEFAULT NULL,
+  total_runs int DEFAULT NULL,
+  player_dismissed text,
+  dismissal_kind text,
+  fielder text
+)
+
+COPY deliveries
+FROM '/tmp/deliveries.csv'
 WITH (FORMAT csv, HEADER true);
